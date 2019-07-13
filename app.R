21a9d8a57df0a328e7d91750d986730562e28bab
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-07-13 16:05:32 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,49 +1,79 @@
-library(dplyr)#minería
+library(dplyr)#minería y exploración
 library(ggplot2)#gráficos
 library(plotly)#gráficos 3D
 library(shinydashboard)#app
 library(shiny)#app
 library(shinyWidgets)#app
-library(Rtsne)#Reducción de la dimensionialidad
+library(Rtsne)#Reducción de la dimensionialidad con algoritmo ML
 
 d <- read.csv('data_unsupervised.csv')
+
 
 d$Lider.Generacion_num <- ifelse(d$Lider.Generacion=="ARC",1,ifelse(d$Lider.Generacion=="GAP",2,3))
 d$Interventoria_num <- ifelse(d$Interventoria=="COLSERING",1,2)
 d$Valor=as.numeric(ifelse(d$DLI_VALOR_TOTAL.x<1000000,1000000, d$DLI_VALOR_TOTAL.x))
-d_num <- d[,c(-12,-17:-18)]
 
-ui <- 
-  fluidPage(
-    setBackgroundColor(color = "WhiteSmoke  ", gradient = c("linear",
-                                                          "radial"), direction = c("bottom", "top", "right", "left")),
-    h1(tags$b("Algoritmos de Aprendizaje no Supervizado t-SNE y PCA")),
-    h4("Por favor espere mientras se realiza el computo en tiempo real"),
-    
+d_num <- d[,c(-13,-18:-19)]
+
+
+ ui <- 
+
+  fluidPage(theme = "bootstrap.min.css",
+      h1(tags$b("Algoritmos de Aprendizaje no Supervizado t-SNE y PCA")),
     tabsetPanel(
       
-      tabPanel("t-SNE",
-               selectInput('col', "Color", colnames(d_num),selected = "Valor"),
-               selectInput('cex', "Tamaño", colnames(d_num),selected = "ONI"),
+      tabPanel("Algortimos t-SNE y k-means",
+               div(style="display:inline-block",  selectInput('col', "Variable para el color", colnames(d_num),selected = "Valor")),
+               div(style="display:inline-block",selectInput('cex', "Variable para el tamaño", colnames(d_num),selected = "ONI")),
                
                
-  plotOutput("plot3",height = '600px'),
+               
+  plotOutput("plot3",height = '550px'),
   h5("Agrupación resultante por el algorimo k-means"),
-  numericInput("cluster","Número de clusters (k)",value = 10),
+  div(style="display:inline-block",numericInput("cluster","Número de clusters (k)",value = 10)),
+  
   textOutput("plot1"),
-  textOutput("plot2")),
+  textOutput("plot2")
   
-  tabPanel("PCA",
-           selectInput('col2', "Color", colnames(d_num[,c(1:15,20)]),selected = "ONI"),
-           sliderInput("size","Tamaño",0.1,1,value = 0.3),
-           plotlyOutput("plot4",height = '800px'),
-           selectInput('cex2', "Tamaño", colnames(d_num[,c(1:15,20)]),selected = "DLI_PESO_A_PAGAR.x")
+  ),
+
   
+  tabPanel("Análisis de Componentes Principales 3D",
+           div(style="display:inline-block", selectInput('col2', "Variable para el color", colnames(d_num[,c(1:16,21)]),selected = "ONI")),
+           div(style="display:inline-block",selectInput('cex2', "Variable para el tamaño", colnames(d_num[,c(1:16,21)]),selected = "DLI_PESO_A_PAGAR.x")),
+           plotlyOutput("plot4",height = '550px'),
+           div(style="display:inline-block",sliderInput("size","Tamaño del punto",0.1,1,value = 0.3)),
+           h5("Resultado de los tres primeros componentes o dimensiones del Algoritmo 'PCA'. Además, se muestran dos dimensiones más, expresadas como color y tamaño de los puntos")
+           
+  
+  ),
+  
+  tabPanel("Referencias y notas",
+           br(),
+           br(),
+           br(),
+         h4("PCA: Análisis de Componentes Principales para cuyo computo se utilizaron los valores 'eigen' de la matriz de correlación de los datos. Se logró reducir la dimensionalidad de la varianza total de la data en un 80%"),
+         br(),
+         h4("tsne: Se construyó para obtener una representación en dos dimensiones de los datos utilizando distancias obtenidas con PCA. Esta técnica es superior para la reducción de la dimensionalidad."),
+         br(),
+         h4("Forgy, E. W. (1965). Cluster analysis of multivariate data: efficiency vs interpretability of classifications. Biometrics, 21, 768–769. "),
+         br(),
+         h4("Hartigan, J. A. and Wong, M. A. (1979). Algorithm AS 136: A K-means clustering algorithm. Applied Statistics, 28, 100–108. doi: 10.2307/2346830. "),
+         br(),
+         h4("Lloyd, S. P. (1957, 1982). Least squares quantization in PCM. Technical Note, Bell Laboratories. Published in 1982 in IEEE Transactions on Information Theory, 28, 128–137."),
+         br(),
+         h4("MacQueen, J. (1967). Some methods for classification and analysis of multivariate observations. In Proceedings of the Fifth Berkeley Symposium on Mathematical Statistics and Probability, eds L. M. Le Cam & J. Neyman, 1, pp. 281–297. Berkeley, CA: University of California Press."),
+         br(),
+         h4("Maaten, L. Van Der, 2014. Accelerating t-SNE using Tree-Based Algorithms. Journal of Machine Learning Research, 15, p.3221-3245."),
+         br(),
+         h4("van der Maaten, L.J.P. & Hinton, G.E., 2008. Visualizing High-Dimensional Data Using t-SNE. Journal of Machine Learning Research, 9, pp.2579-2605."),
+         br(),
+         h4("Mardia, K. V., J. T. Kent and J. M. Bibby (1979). Multivariate Analysis, London: Academic Press.Venables")
   )
+  
+  
     ),
   
- 
-  h5("Resultado de los tres primeros componentes o dimensiones del Algoritmo 'PCA'. Además, se muestran dos dimensiones más, expresadas como color y tamaño de los puntos"),
   
   tags$head(tags$style("#plot1{color: green;
                                  font-size: 20px;
@@ -61,8 +91,11 @@ ui <-
   
   )
     
-server <- function(input, output) {
 
+server <- function(input, output) {
+  
+ 
+  
   output$plot1<-    renderText({
     
     set.seed(7)
@@ -73,6 +106,7 @@ server <- function(input, output) {
 paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
    
    })
+  
   
   output$plot2<-    renderText({
     
@@ -111,15 +145,22 @@ paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
      scale_color_gradient(low = "red", high = "green")+
      scale_shape_manual(values=1:11)
    
+ 
+    
+   
    })
+  
+  
   
   output$plot4<-    renderPlotly({
    
     
       set.seed(7)
       
-      d_tsne_1 <- princomp(d_num[,c(1:15,20)], cor = T)
-   
+      d_tsne_1 <- princomp(d_num[,c(1:16,21)], cor = T)
+      
+     
+     
       set.seed(7)
       
       p <- plot_ly(as.data.frame(d_tsne_1$scores), x = ~d_tsne_1$scores[,1], 
@@ -134,9 +175,11 @@ paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
                             zaxis = list(title = 'Comp. 3 (13 %)')))%>%
                       layout(autosize = T,margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4))
       p
-        
+    
+          
   })
  
 }
 
 shinyApp(ui, server)
+
