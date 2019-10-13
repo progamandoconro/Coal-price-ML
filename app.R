0b74aa27b21ec35c2f7eb80269c49cc03c416e14
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-13 16:28:38 +0100

    Update app.R

--- app.R
+++ app.R
@@ -8,26 +8,21 @@ library(Rtsne)#Reducción de la dimensionialidad con algoritmo ML
 
 d <- read.csv('data_unsupervised.csv')
 
-
 d$Lider.Generacion_num <- ifelse(d$Lider.Generacion=="ARC",1,ifelse(d$Lider.Generacion=="GAP",2,3))
 d$Interventoria_num <- ifelse(d$Interventoria=="COLSERING",1,2)
 d$Valor=as.numeric(ifelse(d$DLI_VALOR_TOTAL.x<1000000,1000000, d$DLI_VALOR_TOTAL.x))
 
 d_num <- d[,c(-13,-18:-19)]
 
-
  ui <- 
 
   fluidPage(theme = "bootstrap.min.css",
       h1(tags$b("Algoritmos de Aprendizaje no Supervizado t-SNE y PCA")),
-    tabsetPanel(
-      
-      tabPanel("Algortimos t-SNE y k-means",
+      tabsetPanel(
+          tabPanel("Algortimos t-SNE y k-means",
                div(style="display:inline-block",  selectInput('col', "Variable para el color", colnames(d_num),selected = "Valor")),
                div(style="display:inline-block",selectInput('cex', "Variable para el tamaño", colnames(d_num),selected = "ONI")),
-               
-               
-               
+                              
   plotOutput("plot3",height = '550px'),
   h5("Agrupación resultante por el algorimo k-means"),
   div(style="display:inline-block",numericInput("cluster","Número de clusters (k)",value = 10)),
@@ -36,7 +31,6 @@ d_num <- d[,c(-13,-18:-19)]
   textOutput("plot2")
   
   ),
-
   
   tabPanel("Análisis de Componentes Principales 3D",
            div(style="display:inline-block", selectInput('col2', "Variable para el color", colnames(d_num[,c(1:16,21)]),selected = "ONI")),
@@ -45,7 +39,6 @@ d_num <- d[,c(-13,-18:-19)]
            div(style="display:inline-block",sliderInput("size","Tamaño del punto",0.1,1,value = 0.3)),
            h5("Resultado de los tres primeros componentes o dimensiones del Algoritmo 'PCA'. Además, se muestran dos dimensiones más, expresadas como color y tamaño de los puntos")
            
-  
   ),
   
   tabPanel("Referencias y notas",
@@ -70,12 +63,10 @@ d_num <- d[,c(-13,-18:-19)]
          br(),
          h4("Mardia, K. V., J. T. Kent and J. M. Bibby (1979). Multivariate Analysis, London: Academic Press.Venables")
   )
-  
-  
+    
     ),
   
-  
-  tags$head(tags$style("#plot1{color: green;
+    tags$head(tags$style("#plot1{color: green;
                                  font-size: 20px;
                                  font-style: bold;  
                                  }"
@@ -94,8 +85,6 @@ d_num <- d[,c(-13,-18:-19)]
 
 server <- function(input, output) {
   
- 
-  
   output$plot1<-    renderText({
     
     set.seed(7)
@@ -111,20 +100,15 @@ paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
   output$plot2<-    renderText({
     
     set.seed(7)
-    
     k <- kmeans(d_num,input$cluster)
-    
     paste("Datos no agrupados:", round( 100-((k$betweenss/k$totss)*100),2), "%")
     
   })
   
   output$plot3<-    renderPlot({
     
-
     set.seed(7)
-    
     d_tsne_1 <- Rtsne(d_num, pca = T, pca_center = T,pca_scale = T)
-    
     set.seed(7)
     km <- kmeans(d_tsne_1$Y,input$cluster)
     
@@ -144,23 +128,13 @@ paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
     labs(col=input$col,Año='Año',cex=input$cex)+ 
      scale_color_gradient(low = "red", high = "green")+
      scale_shape_manual(values=1:11)
-   
- 
-    
-   
+  
    })
-  
-  
   
   output$plot4<-    renderPlotly({
    
-    
       set.seed(7)
-      
       d_tsne_1 <- princomp(d_num[,c(1:16,21)], cor = T)
-      
-     
-     
       set.seed(7)
       
       p <- plot_ly(as.data.frame(d_tsne_1$scores), x = ~d_tsne_1$scores[,1], 
@@ -175,8 +149,7 @@ paste("Datos agrupados:",round( (k$betweenss/k$totss)*100,2), "%")
                             zaxis = list(title = 'Comp. 3 (13 %)')))%>%
                       layout(autosize = T,margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4))
       p
-    
-          
+     
   })
  
 }
